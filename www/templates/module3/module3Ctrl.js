f8412201f31d30c14587cefc5b4261e2e4e265a5
Administrator <a4511@163.com>
2017-12-15 13:00:20 +0800

    提交文件

--- www/templates/module3/module3Ctrl.js
+++ www/templates/module3/module3Ctrl.js
@@ -1,8 +1,19 @@
-
-app.registerController('module3Ctrl', function($scope,getData,$controller) {
-    $controller('BaseViewCtrl',{$scope: $scope});
+app.registerController('module3Ctrl', function ($scope, getData, $controller) {
+    $controller('BaseViewCtrl', {$scope: $scope});
     console.log('加载module4Ctrl');
-    $(function() {
+
+    $scope.textName = {};
+
+    $scope.textareaContNumber = function(){
+        if(textName.key){
+            return textName.key.length;
+        }else{
+            return 0
+        }
+    };
+
+    $scope.$on('$ionicView.afterEnter', function () {
+
         /**
          * 通过数组id获取地址列表数组
          *
@@ -12,7 +23,7 @@ app.registerController('module3Ctrl', function($scope,getData,$controller) {
         function getAddrsArrayById(id) {
             var results = [];
             if (addr_arr[id] != undefined)
-                addr_arr[id].forEach(function(subArr) {
+                addr_arr[id].forEach(function (subArr) {
                     results.push({
                         key: subArr[0],
                         val: subArr[1]
@@ -23,6 +34,7 @@ app.registerController('module3Ctrl', function($scope,getData,$controller) {
             }
             return results;
         }
+
         /**
          * 通过开始的key获取开始时应该选中开始数组中哪个元素
          *
@@ -33,7 +45,7 @@ app.registerController('module3Ctrl', function($scope,getData,$controller) {
         function getStartIndexByKeyFromStartArr(startArr, key) {
             var result = 0;
             if (startArr != undefined)
-                startArr.forEach(function(obj, index) {
+                startArr.forEach(function (obj, index) {
                     if (obj.key == key) {
                         result = index;
                         return false;
@@ -43,12 +55,12 @@ app.registerController('module3Ctrl', function($scope,getData,$controller) {
         }
 
         //bind the click event for 'input' element
-        $("#myAddrs").click(function() {
+        $("#myAddrs").click(function () {
             var PROVINCES = [],
                 startCities = [],
                 startDists = [];
             //Province data，shall never change.
-            addr_arr[0].forEach(function(prov) {
+            addr_arr[0].forEach(function (prov) {
                 PROVINCES.push({
                     key: prov[0],
                     val: prov[1]
@@ -93,7 +105,7 @@ app.registerController('module3Ctrl', function($scope,getData,$controller) {
             }];
             SCS.init({
                 navArr: navArr,
-                onOk: function(selectedKey, selectedValue) {
+                onOk: function (selectedKey, selectedValue) {
                     $input.val(selectedValue).attr("data-key", selectedKey);
                 }
             });
@@ -106,7 +118,7 @@ app.registerController('module3Ctrl', function($scope,getData,$controller) {
                 el: "#" + navArr[1].id,
                 dataArr: startCities,
                 startIndex: cityStartIndex,
-                onChange: function(selectedItem, selectedIndex) {
+                onChange: function (selectedItem, selectedIndex) {
                     distScroller.render(getAddrsArrayById(selectedItem.key), 0); //re-render distScroller when cityScroller change
                 }
             });
@@ -114,14 +126,17 @@ app.registerController('module3Ctrl', function($scope,getData,$controller) {
                 el: "#" + navArr[0].id,
                 dataArr: PROVINCES,
                 startIndex: provStartIndex,
-                onChange: function(selectedItem, selectedIndex) { //re-render both cityScroller and distScroller when provScroller change
+                onChange: function (selectedItem, selectedIndex) { //re-render both cityScroller and distScroller when provScroller change
                     cityScroller.render(getAddrsArrayById(selectedItem.key), 0);
                     distScroller.render(getAddrsArrayById(cityScroller.getSelectedItem().key), 0);
                 }
             });
         });
+
     });
 
+    $scope.baomingxihuo = function(){
 
+    }
 
 })
